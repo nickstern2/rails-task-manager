35a97f428f001936b4607ffcf5d82418fbcf8092
nick stern <nicholas.stern2@gmail.com>
2019-07-30 15:09:27 +0800

    logic completed

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,46 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    set_task
+  end
+
+  def update
+    set_task
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    set_task
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
